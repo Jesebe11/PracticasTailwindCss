37a943dd8a9430ddfbee5d1ecd6d248a1806814d
Jesebell Escobar <jesebellinformatica.gmail.com>
2020-08-25 12:49:48 -0400

    Practicas de Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+    }
+  },
+  variants: {opacity: ['responsive', 'hover', 'focus','disabled'],
+
+},
+  plugins: [],
+}
